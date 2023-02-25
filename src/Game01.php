9b4ea02a3fc6dae8821e86148f07eef31ddea17b
Wu Mobius <energy908@gmail.com>
2023-02-25 14:06:17 +0800

    refactor

--- src/Game01.php
+++ src/Game01.php
@@ -8,23 +8,17 @@ class Game01
     protected int $secondPlayerScore = 0;
     public function score()
     {
-        if ($this->secondPlayerScore===1) {
-            return 'Love Fifteen';
+        $lookup=[
+0=>'Love',
+1=>'Fifteen',
+2=>'Thirty',
+3=>'Forty'
+];
+        if ($this->secondPlayerScore>=1) {
+            return "Love {$lookup[$this->secondPlayerScore]}";
         }
-        if ($this->secondPlayerScore===2) {
-            return 'Love Thirty';
-        }
-        if ($this->secondPlayerScore===3) {
-            return 'Love Forty';
-        }
-        if ($this->firstPlayerScore === 1) {
-            return 'Fifteen Love';
-        }
-        if ($this->firstPlayerScore === 2) {
-            return 'Thirty Love';
-        }
-        if ($this->firstPlayerScore === 3) {
-            return 'Forty Love';
+        if ($this->firstPlayerScore >= 1) {
+            return "{$lookup[$this->firstPlayerScore]} Love";
         }
         return 'Love All';
     }
