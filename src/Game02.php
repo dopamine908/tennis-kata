2f5986821e84d05a09e42ecda5cc52248b6f9fd8
Wu Mobius <energy908@gmail.com>
2023-02-27 21:55:10 +0800

    feat: game 02 - Fifteen Love

--- src/Game02.php
+++ src/Game02.php
@@ -4,8 +4,16 @@ namespace Src;
 
 class Game02
 {
+    public int$firstPlayerScore=0;
     public function score()
     {
+        if ($this->firstPlayerScore===1) {
+            return 'Fifteen Love';
+        }
         return 'Love All';
+    }
+    public function addFirstPlayerScore()
+    {
+        $this->firstPlayerScore++;
     }
 }
--- tests/Game02Test.php
+++ tests/Game02Test.php
@@ -12,4 +12,10 @@ class Game02Test extends TestCase
         $game=new Game02();
         $this->assertEquals('Love All', $game->score());
     }
+    public function testFifteenLove()
+    {
+        $game=new Game02();
+        $game->addFirstPlayerScore();
+        $this->assertEquals('Fifteen Love', $game->score());
+    }
 }
