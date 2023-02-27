2104b640f7d0d34802d1cea805556765fcb0dcae
Wu Mobius <energy908@gmail.com>
2023-02-27 22:09:02 +0800

    feat: game 02 - Love Fifteen

--- src/Game02.php
+++ src/Game02.php
@@ -5,6 +5,7 @@ namespace Src;
 class Game02
 {
     public int$firstPlayerScore=0;
+    public int$secondPlayerScore=0;
     public function score()
     {
         if ($this->firstPlayerScore===1) {
@@ -16,10 +17,17 @@ class Game02
         if ($this->firstPlayerScore===3) {
             return 'Forty Love';
         }
+        if ($this->secondPlayerScore===1) {
+            return 'Love Fifteen';
+        }
         return 'Love All';
     }
     public function addFirstPlayerScore()
     {
         $this->firstPlayerScore++;
+    }
+    public function addSecondPlayerScore()
+    {
+        $this->secondPlayerScore++;
     }
 }
