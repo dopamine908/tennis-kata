41886b6ac8de96716f619fdae3854ac2461c647d
Wu Mobius <energy908@gmail.com>
2023-03-01 23:14:37 +0800

    feat: game 03 - Forty All

--- tests/Game03Test.php
+++ tests/Game03Test.php
@@ -25,4 +25,12 @@ class Game03Test extends TestCase
         $game->addFirstPlayerScore();
         $this->assertEquals('Thirty Love', $game->score());
     }
+    public function testFortyLove()
+    {
+        $game=new Game03();
+        $game->addFirstPlayerScore();
+        $game->addFirstPlayerScore();
+        $game->addFirstPlayerScore();
+        $this->assertEquals('Forty Love', $game->score());
+    }
 }
