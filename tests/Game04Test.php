dd6b4f2f25134b49bffd51289a4b519502942659
Wu Mobius <energy908@gmail.com>
2023-03-13 19:40:17 +0800

    game 04 - first player adv 2

--- tests/Game04Test.php
+++ tests/Game04Test.php
@@ -117,4 +117,19 @@ class Game04Test extends TestCase
         $game->addSecondPlayerScore();
         $this->assertEquals('First Player Adv', $game->score());
     }
+
+    public function testFirstPlayerAdv2()
+    {
+        $game=new Game04();
+        $game->addFisrstPlayerScore();
+        $game->addFisrstPlayerScore();
+        $game->addFisrstPlayerScore();
+        $game->addFisrstPlayerScore();
+        $game->addFisrstPlayerScore();
+        $game->addSecondPlayerScore();
+        $game->addSecondPlayerScore();
+        $game->addSecondPlayerScore();
+        $game->addSecondPlayerScore();
+        $this->assertEquals('First Player Adv', $game->score());
+    }
 }
