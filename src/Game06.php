4a90491543f512796ea7e057a7fbe2def2c4a7f5
Wu Mobius <energy908@gmail.com>
2023-04-02 22:44:58 +0800

    game 06 - fifteen love

--- src/Game06.php
+++ src/Game06.php
@@ -5,8 +5,19 @@ namespace Src;
 class Game06
 {
 
+    private $firstPlayerScore = 0;
+
     public function score()
     {
+        if ($this->firstPlayerScore === 1) {
+            return 'Fifteen Love';
+        }
+
         return 'Love All';
+    }
+
+    public function addFirstPlayerScore()
+    {
+        $this->firstPlayerScore++;
     }
 }
--- tests/Game06Test.php
+++ tests/Game06Test.php
@@ -17,6 +17,16 @@ class Game06Test extends TestCase
         $this->scoreShouldBe('Love All');
     }
 
+
+    /**
+     * @test
+     */
+    public function fifteen_love()
+    {
+        $this->game->addFirstPlayerScore();
+        $this->scoreShouldBe('Fifteen Love');
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
