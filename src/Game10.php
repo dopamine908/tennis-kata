32de9572e6076a13883ca4d6b8a8b930b74ae63e
Wu Mobius <energy908@gmail.com>
2023-04-06 21:40:07 +0800

    game 10 - thirty love

--- src/Game10.php
+++ src/Game10.php
@@ -6,11 +6,16 @@ class Game10
 {
 
     private $firstPlayerScore = 0;
+    private $scoreLookup = [
+        'Love',
+        'Fifteen',
+        'Thirty',
+    ];
 
     public function score()
     {
-        if ($this->firstPlayerScore === 1) {
-            return 'Fifteen Love';
+        if ($this->firstPlayerScore === 1 || $this->firstPlayerScore === 2) {
+            return "{$this->scoreLookup[$this->firstPlayerScore]} Love";
         }
 
         return 'Love All';
