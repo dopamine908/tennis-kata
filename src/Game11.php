6b9952b58cc8553dd648875bb2c3175df00036c9
Wu Mobius <energy908@gmail.com>
2023-04-07 20:40:33 +0800

    game 11 - thirty Love

--- src/Game11.php
+++ src/Game11.php
@@ -13,6 +13,11 @@ class Game11
             return 'Fifteen Love';
         }
 
+        if ($this->firstPlayerScore === 2) {
+            return 'Thirty Love';
+        }
+
+
         return 'Love All';
     }
 
