f8a16d5e992bc82c93d1664980a850d395c0d3e6
Wu Mobius <energy908@gmail.com>
2023-04-07 21:08:11 +0800

    game 11 - second player win

--- tests/Game11Test.php
+++ tests/Game11Test.php
@@ -130,6 +130,16 @@ class Game11Test extends TestCase
         $this->scoreShouldBe('First Player Win');
     }
 
+    /**
+     * @test
+     */
+    public function second_player_win()
+    {
+        $this->givenDeuce();
+        $this->givenSecondPlayerScore(2);
+        $this->scoreShouldBe('Second Player Win');
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
