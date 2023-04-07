e873bca6cdacca8ab82b6eae1ee75c730998b3ac
Wu Mobius <energy908@gmail.com>
2023-04-07 20:47:30 +0800

    game 11 - love fifteen

--- src/Game11.php
+++ src/Game11.php
@@ -6,6 +6,7 @@ class Game11
 {
 
     private $firstPlayerScore = 0;
+    private $secondPayerScore = 0;
     private $scoreLookup = [
         'Love',
         'Fifteen',
@@ -13,12 +14,16 @@ class Game11
         'Forty',
     ];
 
+
     public function score()
     {
         if ($this->firstPlayerScore >= 1) {
             return "{$this->scoreLookup[$this->firstPlayerScore]} Love";
         }
 
+        if ($this->secondPayerScore >= 1) {
+            return "Love {$this->scoreLookup[$this->secondPayerScore]}";
+        }
 
         return 'Love All';
     }
@@ -26,5 +31,10 @@ class Game11
     public function addFirstPlayerScore()
     {
         $this->firstPlayerScore++;
+    }
+
+    public function addSecondPlayerScore()
+    {
+        $this->secondPayerScore++;
     }
 }
