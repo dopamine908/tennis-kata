0e98f884af883525bf191e336cacfb50dd4b91f2
Wu Mobius <energy908@gmail.com>
2023-04-08 16:55:23 +0800

    game 12 - fifteen love

--- src/Game12.php
+++ src/Game12.php
@@ -5,8 +5,19 @@ namespace Src;
 class Game12
 {
 
+    private $firstPlayerScore = 0;
+
     public function score()
     {
+        if ($this->firstPlayerScore === 1) {
+            return 'Fifteen Love';
+        }
+
         return 'Love All';
+    }
+
+    public function addFirstPlayerScore()
+    {
+        $this->firstPlayerScore++;
     }
 }
--- tests/Game12Test.php
+++ tests/Game12Test.php
@@ -7,14 +7,38 @@ use Src\Game12;
 
 class Game12Test extends TestCase
 {
+    private $game;
+
     /**
      * @test
      */
     public function love_all()
     {
-        $game = new Game12();
-        $this->assertEquals('Love All', $game->score());
+        $this->scoreShouldBe('Love All');
+    }
 
+    /**
+     * @test
+     */
+    public function fifteen_love()
+    {
+        $this->game->addFirstPlayerScore();
+        $this->scoreShouldBe('Fifteen Love');
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->game = new Game12();
+    }
+
+    /**
+     * @param $score
+     * @return void
+     */
+    private function scoreShouldBe($score): void
+    {
+        $this->assertEquals($score, $this->game->score());
     }
 
 }
