b7d9f6199c2c66c857e00ef96cb381107145f482
Wu Mobius <energy908@gmail.com>
2023-04-09 22:02:20 +0800

    game 13 - love all

---
+++ src/Game13.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Src;
+
+class Game13
+{
+
+    public function score()
+    {
+        return 'Love All';
+    }
+}
--- tests/Game13Test.php
+++ tests/Game13Test.php
@@ -3,8 +3,17 @@
 namespace Test;
 
 use PHPUnit\Framework\TestCase;
+use Src\Game13;
 
 class Game13Test extends TestCase
 {
 
+    /**
+     * @test
+     */
+    public function love_all()
+    {
+        $game = new Game13();
+        $this->assertEquals('Love All', $game->score());
+    }
 }
