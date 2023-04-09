cd6e962422829d241a1561450b6a3491b41a1184
Wu Mobius <energy908@gmail.com>
2023-04-09 22:23:00 +0800

    game 13 - second player adv

--- tests/Game13Test.php
+++ tests/Game13Test.php
@@ -110,6 +110,16 @@ class Game13Test extends TestCase
         $this->scoreShouldBe('First Player Adv');
     }
 
+    /**
+     * @test
+     */
+    public function second_player_adv()
+    {
+        $this->givenDeuce();
+        $this->givenSecondPlayerScore(1);
+        $this->scoreShouldBe('Second Player Adv');
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
