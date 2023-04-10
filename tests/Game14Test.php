9ce11c231f7985559f56f7018ef685d69586df02
Wu Mobius <energy908@gmail.com>
2023-04-10 22:41:57 +0800

    game 14 - fifteen love

--- tests/Game14Test.php
+++ tests/Game14Test.php
@@ -7,13 +7,39 @@ use Src\Game14;
 
 class Game14Test extends TestCase
 {
+    private $game;
+
     /**
      * @test
      */
     public function love_all()
     {
-        $game = new Game14();
-        $this->assertEquals('Love All', $game->score());
+        $this->scoreShouldBe('Love All');
 
+    }
+
+    /**
+     * @test
+     */
+    public function fifteen_love()
+    {
+        $this->game->addFirstPlayerScore();
+        $this->scoreShouldBe('Fifteen Love');
+
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->game = new Game14();
+    }
+
+    /**
+     * @param $score
+     * @return void
+     */
+    private function scoreShouldBe($score): void
+    {
+        $this->assertEquals($score, $this->game->score());
     }
 }
