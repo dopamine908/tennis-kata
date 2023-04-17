67277c4a415f560a61bd3f6b347548bcbc5d5513
Wu Mobius <energy908@gmail.com>
2023-04-17 22:37:07 +0800

    game 16 - fifteen love

--- src/Game16.php
+++ src/Game16.php
@@ -5,8 +5,20 @@ namespace Src;
 class Game16
 {
 
+    private $firstPlayerScore = 0;
+
+
     public function score()
     {
+        if ($this->firstPlayerScore === 1) {
+            return 'Fifteen Love';
+        }
+
         return 'Love All';
+    }
+
+    public function addFirstPlayerScore()
+    {
+        $this->firstPlayerScore++;
     }
 }
