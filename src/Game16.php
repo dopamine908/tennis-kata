2e8961cc57ce8f42a7427704c62caed02b4c6eef
Wu Mobius <energy908@gmail.com>
2023-04-17 22:52:05 +0800

    game 16 - deuce

--- src/Game16.php
+++ src/Game16.php
@@ -18,15 +18,11 @@ class Game16
     public function score()
     {
         if ($this->firstPlayerScore === $this->secondPlayerScore) {
-            if ($this->firstPlayerScore === 1) {
-                return 'Fifteen All';
+            if ($this->firstPlayerScore === 3) {
+                return 'Deuce';
             }
 
-            if ($this->firstPlayerScore === 2) {
-                return 'Thirty All';
-            }
-
-            return 'Love All';
+            return "{$this->scoreLookup[$this->firstPlayerScore]} All";
         }
 
         return "{$this->scoreLookup[$this->firstPlayerScore]} {$this->scoreLookup[$this->secondPlayerScore]}";
